9213ce54a73b36ad270ce625161d557420746e15
4533 <4533@tower.corp>
2023-10-09 11:36:48 +0300

    global styles

--- frontend/src/pages/CreateLesson/styles.ts
+++ frontend/src/pages/CreateLesson/styles.ts
@@ -3,6 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const ChangeBodyBg = createGlobalStyle`
   body {
-    background-color: ${(props) => props.theme.colors.realWhite} !important;
+    background-color: ${(props) => props.theme.colors.realWhite};
   }
 `;
