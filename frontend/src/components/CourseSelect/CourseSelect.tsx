e34c0ba9acf3ab93727c068ccd8abb9fe9cf51ad
5070 <5070@tower.corp>
2023-10-19 10:38:03 +0300

    labelSelect

--- frontend/src/components/CourseSelect/CourseSelect.tsx
+++ frontend/src/components/CourseSelect/CourseSelect.tsx
@@ -1,10 +1,11 @@
-import { ADMIN_BTN_TYPES, MODAL_TYPES } from '@/constants';
-import * as S from './styles';
-import { useActions } from '@/hooks/useActions';
-import { CustomSelect, IOption } from '../CustomSelect';
-import { ICourse } from '@/types/course.types';
-import { SingleValue } from 'react-select';
-import { AdminBtn } from '../AdminBtn';
+import { ADMIN_BTN_TYPES, MODAL_TYPES } from "@/constants";
+import * as S from "./styles";
+import { useActions } from "@/hooks/useActions";
+import { CustomSelect, IOption } from "../CustomSelect";
+import { ICourse } from "@/types/course.types";
+import { SingleValue } from "react-select";
+import { AdminBtn } from "../AdminBtn";
+import { CustomSelectOpions } from "../CustomSelectOpions/CustomSelectOpions";
 
 interface ICourseSelectProps {
   data: Array<ICourse>;
@@ -15,10 +16,10 @@ export function CourseSelect({ data }: ICourseSelectProps) {
   const selectOptions = data.map((course) => {
     return {
       value: course.id,
-      label: `Курс: ${course.title}`,
+      // label: `Курс: ${course.title}`,
+      label: <CustomSelectOpions text={course.title} percentage={0} />,
     };
   });
-
 
   const openCreateCourseModal = () => {
     setModalType(MODAL_TYPES.createCourse);
@@ -26,18 +27,19 @@ export function CourseSelect({ data }: ICourseSelectProps) {
   };
 
   const handleChange = (option: SingleValue<IOption>) => {
-    if(!option) return;
+    if (!option) return;
     const activeCourseData = data.find((course) => course.id === option.value);
     setCourseData(activeCourseData);
-  }
+  };
 
   return (
     <S.Container>
-      <CustomSelect onChange={handleChange} options={selectOptions} defaultValue={selectOptions[0]}/>
-      <AdminBtn
-        type={ADMIN_BTN_TYPES.add}
-        onClick={openCreateCourseModal}
+      <CustomSelect
+        onChange={handleChange}
+        options={selectOptions}
+        defaultValue={selectOptions[0]}
       />
+      <AdminBtn type={ADMIN_BTN_TYPES.add} onClick={openCreateCourseModal} />
     </S.Container>
   );
 }
--- frontend/src/components/CustomSelect/styles.ts
+++ frontend/src/components/CustomSelect/styles.ts
@@ -3,6 +3,5 @@ import * as C from "@styles/components";
 import dropdownIcon from "@assets/images/dropdownIcon.svg";
 
 export const dropdownIndicator = styled(C.Icon)`
-  border: none;
   background-image: url(${dropdownIcon});
 `;
