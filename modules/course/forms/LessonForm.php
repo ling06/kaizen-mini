c668b18b95ae21b3b57f3de327fe5702241fde94
CTC <ctc12@mail.ru>
2023-11-10 14:14:51 +0300

    add method to clear check lesson-theme-chapter-course when change lesson

--- modules/course/forms/LessonForm.php
+++ modules/course/forms/LessonForm.php
@@ -4,12 +4,16 @@ namespace app\modules\course\forms;
 
 use app\models\Image;
 use app\modules\course\models\Answer;
+use app\modules\course\models\Chapter;
+use app\modules\course\models\Course;
 use app\modules\course\models\Lesson;
 use app\modules\course\models\Question;
 use app\modules\course\models\Test;
+use app\modules\course\models\Theme;
 use app\modules\course\models\UserCheck;
 use app\modules\course\models\UserTestAnswer;
 use Yii;
+use yii\db\StaleObjectException;
 
 class LessonForm extends Lesson
 {
@@ -46,6 +50,13 @@ class LessonForm extends Lesson
         return parent::beforeSave($insert);
     }
 
+    /**
+     * @param $insert
+     * @param $changedAttributes
+     * @return void
+     * @throws \Throwable
+     * @throws StaleObjectException
+     */
     public function afterSave($insert, $changedAttributes): void
     {
         $editedTests = [];
@@ -54,57 +65,71 @@ class LessonForm extends Lesson
             $editedAnswers = [];
             if (isset($tests['id']) && !empty($tests['id'])) {
                 if ($tests['question'] !== $testsFromDb[$tests['id']]->question) {
-                    UserTestAnswer::deleteAll(['test_question_id' => $tests['id']]);
-                    UserCheck::deleteAll(['model_name' => Lesson::class, 'model_pk' => $this->id]);
+                    self::checkClear($tests['id'], $this->id);
+                    $editedTests[] = $tests['id'];
+                    $test = Test::find()->where(['id' => $tests['id']])->one();
+                } else {
+                    $test = new Test;
                 }
-                $editedTests[] = $tests['id'];
-                $test = Test::find()->where(['id' => $tests['id']])->one();
-            } else {
-                $test = new Test;
-            }
-            $test->lesson_id = $this->id;
-            $test->question = $tests['question'];
-            if ($test->save() && !empty($test->id)) {
-                $answersFromDb = Question::find()->where(['test_id' => $test->id])->all();
-                foreach ($tests['answers'] as $answer) {
-                    $editedAnswers[] = $answer['id'] ?? null;
-                    if (isset($answer['id']) && !empty($answer['id'])) {
-                        $testAnswer = Question::find()->where(['id' => $answer['id']])->one();
-                        if ($testAnswer->right_answer !== $answer['right_answer']) {
-                            UserTestAnswer::deleteAll(['test_question_id' => $tests['id']]);
-                            UserCheck::deleteAll(['model_name' => Lesson::class, 'model_pk' => $this->id]);
+                $test->lesson_id = $this->id;
+                $test->question = $tests['question'];
+                if ($test->save() && !empty($test->id)) {
+                    $answersFromDb = Question::find()->where(['test_id' => $test->id])->all();
+                    foreach ($tests['answers'] as $answer) {
+                        $editedAnswers[] = $answer['id'] ?? null;
+                        if (isset($answer['id']) && !empty($answer['id'])) {
+                            $testAnswer = Question::find()->where(['id' => $answer['id']])->one();
+                            if ($testAnswer->right_answer !== $answer['right_answer']) {
+                                self::checkClear($tests['id'], $this->id);
+                            }
+                        } else {
+                            $testAnswer = new Question;
                         }
-                    } else {
-                        $testAnswer = new Question;
+                        $testAnswer->test_id = $test->id;
+                        $testAnswer->answer = $answer['answer'];
+                        $testAnswer->right_answer = $answer['right_answer'];
+                        $testAnswer->text = $answer['text'];
+                        $testAnswer->save(false);
                     }
-                    $testAnswer->test_id = $test->id;
-                    $testAnswer->answer = $answer['answer'];
-                    $testAnswer->right_answer = $answer['right_answer'];
-                    $testAnswer->text = $answer['text'];
-                    $testAnswer->save(false);
-                }
-                foreach ($answersFromDb as $dbAnswer) {
-                    if (!in_array($dbAnswer->id, $editedAnswers)) {
-                        $dbAnswer->delete();
-                        UserTestAnswer::deleteAll(['test_question_id' => $tests['id']]);
-                        UserCheck::deleteAll(['model_name' => Lesson::class, 'model_pk' => $this->id]);
+                    foreach ($answersFromDb as $dbAnswer) {
+                        if (!in_array($dbAnswer->id, $editedAnswers)) {
+                            $dbAnswer->delete();
+                            self::checkClear($tests['id'], $this->id);
+                        }
                     }
                 }
             }
-        }
-        if ($testsFromDb) {
-            foreach ($testsFromDb as $dbTest) {
-                if (!in_array($dbTest->id, $editedTests)) {
-                    $dbTest->deleteSoft();
+            if ($testsFromDb) {
+                foreach ($testsFromDb as $dbTest) {
+                    if (!in_array($dbTest->id, $editedTests)) {
+                        $dbTest->deleteSoft();
+                    }
                 }
             }
-        }
-        $lesson = Lesson::findOne($this->id);
-        if ($lesson->description) {
-            $lesson->description = json_encode(Image::saveEditorJsImage(json_decode($lesson->description), 'LessonsEditorJS', $this->id), JSON_UNESCAPED_UNICODE);
-        }
-        $lesson->save();
+            $lesson = Lesson::findOne($this->id);
+            if ($lesson->description) {
+                $lesson->description = json_encode(Image::saveEditorJsImage(json_decode($lesson->description), 'LessonsEditorJS', $this->id), JSON_UNESCAPED_UNICODE);
+            }
+            $lesson->save();
 
-        parent::afterSave($insert, $changedAttributes);
+            parent::afterSave($insert, $changedAttributes);
+        }
+    }
+
+    /**
+     * @param $testId
+     * @param $lessonId
+     * @return void
+     */
+    public static function checkClear($testId, $lessonId)
+    {
+        UserTestAnswer::deleteAll(['test_question_id' => $testId]);
+        UserCheck::deleteAll(['model_name' => Lesson::class, 'model_pk' => $lessonId]);
+        $themeId = Lesson::find()->where(['id' => $lessonId])->one()->theme_id;
+        UserCheck::deleteAll(['model_name' => Theme::class, 'model_pk' => $themeId]);
+        $chapterId = Theme::find()->where(['id' => $themeId])->one()->chapter_id;
+        UserCheck::deleteAll(['model_name' => Chapter::class, 'model_pk' => $chapterId]);
+        $courseId = Chapter::find()->where(['id' => $chapterId])->one()->course_id;
+        UserCheck::deleteAll(['model_name' => Course::class, 'model_pk' => $courseId]);
     }
 }
