1573b5a702751a6fb1bb415b01d1c5a0203ea1f9
Trishin Veniamin <ling06@yandex.ru>
2023-11-13 13:03:25 +0300

    + хук на автопулл из гита

--- web/pull-from-git.php
+++ web/pull-from-git.php
@@ -1,3 +1,26 @@
 <?php
-// Pu11FromGtiN0\/\/
-file_put_contents('pull-from-git.txt', print_r($_REQUEST));
+// Получаем заголовки и тело запроса
+$headers = getallheaders();
+$payload = file_get_contents("php://input");
+
+// Проверяем сигнатуру HMAC с использованием секретного ключа
+$secret = 'Pu11FromGtiN0\/\/';
+$signature = 'sha1=' . hash_hmac('sha1', $payload, $secret);
+
+// Проверяем, что это событие push и сигнатура совпадает
+if ($headers['X-GitHub-Event'] === 'push' && $headers['X-Hub-Signature'] === $signature) {
+    // Выполняем необходимые действия, например, обновление кода и перезапуск сервера
+    // В данном примере, мы будет использовать команду git pull для обновления кода
+    shell_exec('cd /var/www/kaizen/www/ && git pull');
+
+    // Дополнительные действия, если это необходимо
+    // ...
+
+    // Отправляем ответ об успешном выполнении
+    http_response_code(200);
+    echo "Webhook processed successfully";
+} else {
+    // Отправляем ошибку, если сигнатура не совпадает
+    http_response_code(401);
+    echo "Invalid signature";
+}
