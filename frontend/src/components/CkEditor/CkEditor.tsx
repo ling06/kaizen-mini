7c2070fb2ded31f6a62577e0e065ae6f349c9610
4533 <4533@tower.corp>
2023-11-13 14:08:52 +0300

    before i npm-check-updates

--- frontend/src/components/CkEditor/CkEditor.tsx
+++ frontend/src/components/CkEditor/CkEditor.tsx
@@ -1,36 +1,41 @@
 import * as S from './styles';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
-import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import '../../../ckEditor5/build/ckeditor'
+// import Editor from 'ckeditor5-custom-build/build/ckeditor';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
+// import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
+const configEditor = {
+  simpleUpload: {
+    uploadUrl: '/api/course/upload-temp-image',
+  },
+}
 
 export function CkEditor({ onChange }) {
   return (
-    <CKEditor
-      editor={Editor}
-      config={
-        {
-          simpleUpload: {
-            uploadUrl: 'http://example.com',
-          }
-        }
-      }
-      data="<p>Hello from CKEditor&nbsp;5!</p>"
-      onReady={(editor) => {
-        console.log('Editor is ready to use!', editor);
-      }}
-      onChange={(event, editor) => {
-        const data = editor.getData();
-        onChange(data);
-      }}
-      onBlur={(event, editor) => {
-        console.log('Blur.', editor);
-      }}
-      onFocus={(event, editor) => {
-        console.log('Focus.', editor);
-      }}
-    />
+    <>
+
+<CKEditor
+  editor={ClassicEditor}
+  config={
+    configEditor
+  }
+  data="<p>Hello from CKEditor&nbsp;5!</p>"
+  onReady={(editor) => {
+    console.log('Editor is ready to use!', editor);
+  }}
+  onChange={(event, editor) => {
+    const data = editor.getData();
+    onChange(data);
+  }}
+  onBlur={(event, editor) => {
+    console.log('Blur.', editor);
+  }}
+  onFocus={(event, editor) => {
+    console.log('Focus.', editor);
+  }}
+/>
+    </>
   );
 }
