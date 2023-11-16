eb4a1ea259b02a29c8427deaea498867aa8025d5
4533 <4533@tower.corp>
2023-11-16 12:38:50 +0300

    config editor

--- frontend/src/components/CkEditor/CkEditor.tsx
+++ frontend/src/components/CkEditor/CkEditor.tsx
@@ -1,69 +1,18 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import '../../../ckEditor5/build/ckeditor'
 import Editor from 'ckeditor5-custom-build';
-// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
-// import { UploadAdapter, FileLoader } from '@ckeditor/ckeditor5-upload/src/filerepository';
-// import { Editor } from '@ckeditor/ckeditor5-core';
 import { useEffect, useState } from 'react';
-// import axios from 'axios';
 import * as S from './styles';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 
-// import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
-
-// function uploadAdapter(loader: FileLoader): UploadAdapter {
-//   const csrf = document.querySelector('meta[name="csrf-token"]').content;
-//   console.log(csrf);
-
-//   return {
-//     upload: () => {
-//       return new Promise(async (resolve, reject) => {
-//         try {
-//           const file = await loader.file;
-//           const response = await axios.request({
-//             method: 'POST',
-//             url: `/api/course/upload-temp-image`,
-//             data: {
-//               image: file,
-//             },
-//             headers: {
-//               'Content-Type': 'multipart/form-data',
-//               'X-CSRF-Token': csrf,
-//             },
-//           });
-//           resolve({
-//             default: response.data.file.url,
-//           });
-//         } catch (error) {
-//           reject('Hello');
-//         }
-//       });
-//     },
-//     abort: () => {},
-//   };
-// }
-// function uploadPlugin(editor: Editor) {
-//   editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-//     return uploadAdapter(loader);
-//   };
-// }
-
 interface ICkEditorProps {
   onChange: (data: string) => void;
-  data: string;
+  data?: string;
   type: string;
 }
 
-export function CkEditor({ onChange, data, type }: ICkEditorProps) {
-  const [editor, setEditor] = useState('');
+export function CkEditor({ onChange, data="" }: ICkEditorProps) {
+  const [editor, setEditor] = useState(data);
   const { token } = useTypedSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (data && type === 'edit') {
-      setEditor(data);
-    }
-  }, [data, type]);
 
   return (
     <S.CkEditorContainer>
@@ -83,18 +32,17 @@ export function CkEditor({ onChange, data, type }: ICkEditorProps) {
           },
         }}
         onReady={(editor) => {
-          console.log('Editor is ready to use!', editor);
+          console.log('Editor is ready to use!');
         }}
         onChange={(event, editor) => {
           setEditor(editor.getData());
           onChange(editor.getData());
         }}
         onBlur={(event, editor) => {
-          // console.log('Blur.', editor);
+          setEditor(editor.getData());
+          onChange(editor.getData());
         }}
-        onFocus={(event, editor) => {
-          // console.log('Focus.', editor);
-        }}
+        onError={(error) => console.error('Error in CkEditor:', error)}
       />
     </S.CkEditorContainer>
   );
