95b2c289a856feae30f593a344ebf85c1dcb7d4a
4533 <4533@tower.corp>
2023-11-22 11:26:03 +0300

    refactor: singled out Progress to component from CourseProgrammCard

--- frontend/src/components/CourseProgramm/CourseProgrammCard/styles.ts
+++ frontend/src/components/CourseProgramm/CourseProgrammCard/styles.ts
@@ -55,32 +55,3 @@ export const Title = styled(C.Text)`
     font-size: 3.75vw;
   }
 `;
-
-export const ProgressContainer = styled(C.FlexContainer)`
-  flex-direction: column;
-  margin-top: auto;
-`;
-
-export const ProgressStatusWrapper = styled(C.FlexContainer)`
-  align-items: flex-end;
-  margin-bottom: 10px;
-  @media ${(props) => props.theme.media.mobile} {
-    margin-bottom: 1.875vw;
-  }
-`;
-
-export const ProgressStatus = styled.p`
-  margin-right: auto;
-  font-size: 15px;
-  font-weight: 500;
-  line-height: 160%;
-  color: ${(props) => props.theme.colors.realBlack};
-  @media ${(props) => props.theme.media.mobile} {
-    font-size: 2.5vw;
-  }
-`;
-
-export const BtnsGroup = styled(C.FlexContainer)`
-  flex-direction: column;
-  width: fit-content;
-`;
---
+++ frontend/src/components/CourseProgramm/Progress/Progress.tsx
@@ -0,0 +1,88 @@
+import { AdminBtn } from '@/components/AdminBtn';
+import { DndBtn } from '@/components/DndBtn';
+import { ADMIN_BTN_TYPES, MODAL_TYPES } from '@/constants';
+import { css } from 'styled-components';
+import * as S from './styles';
+import * as C from '@styles/components';
+import { useCallback } from 'react';
+import dndIconWithArrows from '@assets/images/dnd-btn-arrows.svg';
+import dndIconWithArrowsActive from '@assets/images/dnd-btn-arrows-active.svg';
+import { useActions } from '@/hooks/useActions';
+import { useDeleteChapterMutation, useRestoreChapterMutation } from '@/store/api/chapter.api';
+import { IChapter } from '@/types/chapter.types';
+
+interface IProgressProps {
+  data: IChapter;
+  setDraggable: () => void;
+  setNotDraggable: () => void;
+  setDeleted: (arg: boolean) => void;
+  isDeleted: boolean;
+}
+
+
+export function Progress({ data, setDraggable, setNotDraggable, setDeleted, isDeleted }: IProgressProps) {
+  const { setLoaderActive, setModalOpen, setModalType, setUpdatingChapterData } = useActions();
+  const [deleteChapter] = useDeleteChapterMutation();
+  const [restoreChapter] = useRestoreChapterMutation();
+  const setProgressStatus = useCallback(() => {
+    if (data.percentage.percentage == 100) {
+      return 'Пройдено';
+    } else if (data.percentage.percentage > 0) {
+      return 'В процессе';
+    }
+    return 'Предстоит';
+  }, [data.percentage.percentage]);
+
+  const handleDeleteChapter = () => {
+    deleteChapter({ id: data.id }).then(() => {
+      setLoaderActive(false);
+      setDeleted(true);
+    });
+    setLoaderActive(true);
+  };
+
+  const handleRestoreChapter = () => {
+    restoreChapter({ id: data.id }).then(() => {
+      setLoaderActive(false);
+      setDeleted(false);
+    });
+    setLoaderActive(true);
+  };
+
+  const handleEditChapter = () => {
+    setModalType(MODAL_TYPES.editChapter);
+    setUpdatingChapterData(data);
+    setModalOpen(true);
+  };
+
+  return (
+    <S.ProgressContainer>
+    <S.ProgressStatusWrapper>
+      <S.ProgressStatus>{setProgressStatus()}</S.ProgressStatus>
+      <S.BtnsGroup>
+        <DndBtn
+          styles={css`
+            background-image: url(${dndIconWithArrows});
+            &:hover {
+              background-image: url(${dndIconWithArrowsActive});
+            }
+          `}
+          onMouseEnter={setDraggable}
+          onMouseLeave={setNotDraggable}
+        />
+        <AdminBtn
+          popupName="Глава"
+          type={ADMIN_BTN_TYPES.edit}
+          onClick={() => {}}
+          popupHandlers={{
+            onDelete: isDeleted ? undefined : handleDeleteChapter,
+            onRestore: isDeleted ? handleRestoreChapter : undefined,
+            onEdit: handleEditChapter,
+          }}
+        />
+      </S.BtnsGroup>
+    </S.ProgressStatusWrapper>
+    <C.ProgressBar $progress={String(data?.percentage.percentage) || '0'} />
+  </S.ProgressContainer>
+  );
+}
